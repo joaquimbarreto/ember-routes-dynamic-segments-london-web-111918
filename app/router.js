f76b10f50d935a04de4f161fbf4e9f27061cc503
Joaquim Barreto <joaquim@barreto.co>
2019-04-14 12:22:36 +0100

    Done

--- app/router.js
+++ app/router.js
@@ -1,12 +1,14 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 const Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
-  
+  this.route("artists", function() {
+    this.route("artist", { path: ":artist_id" });
+  });
 });
 
 export default Router;
